197b263f0c4868efedfa72d8a62da9ffc983e70f
satabios <max.satabiossathya@gmail.com>
2024-05-21 16:48:36 -0700

    updated all

---
+++ dataset.py
@@ -0,0 +1,31 @@
+from torch.utils.data import DataLoader
+from torchvision.datasets import *
+from torchvision.transforms import *
+
+image_size = 32
+transforms = {
+    "train": Compose([
+        RandomCrop(image_size, padding=4),
+        RandomHorizontalFlip(),
+        ToTensor(),
+    ]),
+    "test": ToTensor(),
+}
+dataset = {}
+for split in ["train", "test"]:
+    dataset[split] = CIFAR10(
+        root="data/cifar10",
+        train=(split == "train"),
+        download=True,
+        transform=transforms[split],
+    )
+dataloader = {}
+for split in ['train', 'test']:
+    dataloader[split] = DataLoader(
+        dataset[split],
+        batch_size=512,
+        shuffle=(split == 'train'),
+        num_workers=0,
+        pin_memory=True,
+    )
+
