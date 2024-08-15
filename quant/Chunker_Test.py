fffded11c5145e36af23d939b46150b428f689f2
satabios <max.satabiossathya@gmail.com>
2024-08-14 19:10:28 -0700

    udpated chunker_test

--- quant/Chunker_Test.py
+++ quant/Chunker_Test.py
@@ -5,35 +5,47 @@ from models import SimpleCNN, VGG
 from Chunker import Chunker
 from tqdm import tqdm
 
+from sconce import sconce
+import torch.nn as nn
+import torch.optim as optim
 
-resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18')
-mbnet = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2')
+# resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18')
+# mbnet = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2')
 vgg = VGG()
-# vgg.load_state_dict(torch.load("vgg.cifar.pretrained.pth"))
+vgg.load_state_dict(torch.load("vgg.cifar.pretrained.pth"))
 # smp = SimpleCNN()
 test_data = torch.rand(1, 3, 128, 128)
 
-for model in tqdm([mbnet, resnet, vgg]):#tqdm([resnet, mbnet, vgg, smp]):
-    test = torch.rand(1, 3, 128, 128)
 
+def evaluate_model(model, test_loader):
+    # Set the model to evaluation mode
+    model.eval()
 
+    # Initialize counters for correct predictions and total samples
+    correct = 0
+    total = 0
 
-    # sconces = sconce()
-    # sconces.model = model  # Model Definition
-    # sconces.criterion = nn.CrossEntropyLoss()  # Loss
-    # sconces.optimizer = optim.Adam(sconces.model.parameters(), lr=1e-4)
-    # sconces.scheduler = optim.lr_scheduler.CosineAnnealingLR(sconces.optimizer, T_max=200)
-    # sconces.dataloader = dataloader
-    # sconces.epochs = 5  # Number of time we iterate over the data
-    # sconces.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # print(sconces.evaluate(device='cpu'))
+    # Disable gradient computation since we are in inference mode
+    with torch.no_grad():
+        for data in tqdm(test_loader, desc="Evaluating", leave=False):
+            inputs, labels = data
+            outputs = model(inputs)
 
+            # Get the predicted class (the one with the highest probability)
+            _, predicted = torch.max(outputs.data, 1)
+
+            # Update the total and correct counters
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # Calculate accuracy
+    accuracy = 100 * correct / total
+
+    return accuracy
+
+for model in tqdm([vgg]):#tqdm([resnet, mbnet, vgg, smp]):
+    print(f"Original Model Accuracy : {evaluate_model(model, dataloader['test'])}")
     fuser = Fuse(model.eval(), dataloader['test'])
     fused_model = fuser.fused_model.train()
-
     quantized_model = Chunker(fused_model, dataloader['test']).model
-
-    _  = quantized_model(test_data)
-    # sconces.model = quantized_model
-    #
-    # print(sconces.evaluate(device='cpu'))
+    print(f"Quantized Model Accuracy : {evaluate_model(quantized_model, dataloader['test'])}")
